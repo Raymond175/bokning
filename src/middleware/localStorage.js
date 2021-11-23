dfd542a021ef4f16f74e0d498aaef55c6db91367
Raymond Jones <raymond99@hotmail.se>
2021-11-23 23:15:01 +0100

    Updates

--- src/middleware/localStorage.js
+++
@@ -1,6 +0,0 @@
-const localStorageMiddleware = ({ getState }) => (next) => (action) => {
-  next(action);
-  localStorage.setItem("state", JSON.stringify(getState()));
-};
-
-export default localStorageMiddleware;
